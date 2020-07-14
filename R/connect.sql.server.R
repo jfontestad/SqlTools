a5fc08e8459fd5081d513b8f11bd1a6aabb33bf5
Nathan Palmer <nathan_palmer@hms.harvard.edu>
2020-07-14 17:17:30 -0400

    fixed more variable names

--- R/connect.sql.server.R
+++ R/connect.sql.server.R
@@ -33,7 +33,7 @@ connect.sql.server <- function (database, user, password, server.address, domain
             server = server.address, 
             port = port, 
             database = database, 
-            uid = paste(sep="", domain, "\\", username), 
+            uid = paste(sep="", domain, "\\", user), 
             pwd = password, 
             TDS_Version = "8.0")
     }
